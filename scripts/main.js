707c68dc8d89222670793a0145baeb3e59f9fc3b
Mark Hill <futonfutonda@gmail.com>
2016-11-22 11:57:31 +0000

    added smooth scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,19 @@ $(window).scroll(function(){
   };
 });
 
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 500, 'swing');
+	});
+});
+
 //nav
 $('#drop-down-icon').click(function(){
   $('.links').slideToggle();
